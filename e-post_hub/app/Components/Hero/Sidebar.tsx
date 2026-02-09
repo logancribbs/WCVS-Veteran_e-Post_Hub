77b85b94ba598e6b32594d5cc412a9387f73af82
logancribbs <logan.cribbs@yahoo.com>
2026-02-09 14:15:57 -0800

    Fixed sidebar gradient rendering to be consistent across differrent operating systems.

--- e-post_hub/app/Components/Hero/Sidebar.tsx
+++ e-post_hub/app/Components/Hero/Sidebar.tsx
@@ -131,6 +131,7 @@ export default function Sidebar() {
         shadow-[0_6px_16px_rgba(0,0,0,0.35)]
         md:sticky md:top-6
         gap-4
+        transform-gpu
       "
     >
       {/* Slideshow */}
